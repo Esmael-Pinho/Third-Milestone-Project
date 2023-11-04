c0d963a775ff98a151f924ae36341c016fec5426
Esmael Pinho <esma.pinho@gmail.com>
2023-11-04 17:27:46 +0000

    cleaned some extra code on css, added flash messages to base.html and added more import to route.py

--- coreshare/routes.py
+++ coreshare/routes.py
@@ -1,7 +1,11 @@
-from flask import render_template
+import datetime
+from flask import flash, render_template, request, redirect, session, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 from coreshare import app, db
+from coreshare.model import User
 
 
 @app.route("/")
 def home():
     return render_template("base.html")
+
