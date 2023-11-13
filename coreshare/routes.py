6ab9c7f2a761e93f15bf411ad55a3c99543a84bc
Esmael Pinho <esma.pinho@gmail.com>
2023-11-13 15:28:17 +0000

    added edit_category route, as well as html file. added modal as extra security for category deletion

--- coreshare/routes.py
+++ coreshare/routes.py
@@ -116,12 +116,26 @@ def add_category():
     return render_template("add_category.html")
 
 
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        category.category_image_url = request.form.get("category_image_url")
+        db.session.commit()
+
+        flash("Category edited successfully!", category="success")
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
+
+
 @app.route("/delete_category/<int:category_id>")
 def delete_category(category_id):
     category = Category.query.get_or_404(category_id)
     db.session.delete(category)
     db.session.commit()
     return redirect(url_for("categories"))
+
 
 
 @app.route("/posts")
