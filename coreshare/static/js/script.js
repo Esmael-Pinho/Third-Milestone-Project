54f6664f98f1d8050508eceb0e6d01c593a1bd1e
Esmael Pinho <esma.pinho@gmail.com>
2023-11-16 05:47:23 +0000

    changed form class name as it was preventing from login

--- coreshare/static/js/script.js
+++ coreshare/static/js/script.js
@@ -10,24 +10,21 @@ $(document).ready(function() {
         $("#contact-form").show();
     });
 
-    // Function to show/hide the message based on the selected value
     function updateCategoryMessage() {
         var selectedCategory = $("#category_id").val();
         if (!selectedCategory || selectedCategory === "Choose Category") {
             $("#categoryMessage").show();
-            // Disable the form submission
-            $("form").attr("onsubmit", "return false;");
+            // Disable the form submission for forms without the "log-reg-cat" class
+            $("form:not(.log-reg-cat)").attr("onsubmit", "return false;");
         } else {
             $("#categoryMessage").hide();
             // Enable the form submission and update the hidden input field
-            $("form").removeAttr("onsubmit");
+            $("form:not(.log-reg-cat)").removeAttr("onsubmit");
             $("#selectedCategory").val(selectedCategory);
         }
-        // Attach the function to the change event of the dropdown
-        $("#category_id").change(updateCategoryMessage);
     }
-
-    // Call the function initially to set the initial state
+    
+    // Call the function to set the initial state
     updateCategoryMessage();
 
 
